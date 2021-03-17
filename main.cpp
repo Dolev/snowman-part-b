e423e2d40bda5ca2cb21710a72634cb465c0349a
Dolev .H <33119992+Dolev@users.noreply.github.com>
2021-03-17 15:07:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 int main()
 {
     
-    std::cout << ariel::snowman(11114411) << std::endl;
-    std::cout << ariel::snowman(11112311) << std::endl;
+    std::cout << ariel::snowman(11114421) << std::endl;
+    std::cout << ariel::snowman(31214411) << std::endl;
+    std::cout << ariel::snowman(23114411) << std::endl;
+    std::cout << ariel::snowman(41143211) << std::endl;
     //std::cout<< s;
 
   //CHECK(nospaces(snowman(11114411)) == nospaces(" _===_ \n (.,.) \n ( : ) \n ( : )"));
